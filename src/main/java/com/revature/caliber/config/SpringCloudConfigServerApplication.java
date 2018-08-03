c46b59adf962a3158d211169578c1de854d1ba0a
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-08-02 21:31:01 -0400

    Lots of tests!!!

--- src/main/java/com/revature/caliber/config/SpringCloudConfigServerApplication.java
+++ src/main/java/com/revature/caliber/config/SpringCloudConfigServerApplication.java
@@ -2,20 +2,19 @@ package com.revature.caliber.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
-	private static String test = "Test";
+	private String test = "Test";
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 	
-	public static String getTest() {
+	public String getTest() {
 		return test;
 	}
 }
--- src/test/java/com/revature/caliber/config/MainTest.java
+++ src/test/java/com/revature/caliber/config/MainTest.java
@@ -2,12 +2,26 @@ package com.revature.caliber.config;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MainTest {
+	
+	static SpringCloudConfigServerApplication app;
+	
+	@BeforeClass
+	public static void createSpring() {
+		app = new SpringCloudConfigServerApplication();
+	}
 
 	@Test
 	public void testMain() {
-		assertEquals("This is a passing test", "Test", SpringCloudConfigServerApplication.getTest());
+		assertEquals("This is a passing test", "Test", app.getTest());
+	}
+	
+	@AfterClass
+	public static void deleteSpring() {
+		app = null;
 	}
 }
