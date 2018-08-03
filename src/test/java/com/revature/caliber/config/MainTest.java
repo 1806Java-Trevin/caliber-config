f78af0b6c053cf7d37a455a1b0971854715bae9d
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-08-02 21:01:47 -0400

    Added a very important test!

---
+++ src/test/java/com/revature/caliber/config/MainTest.java
@@ -0,0 +1,13 @@
+package com.revature.caliber.config;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		assertEquals("This is a passing test", "Test", "Test");
+	}
+}
